0f0ea198b84306cc3df85c13c2882a30196ec6ad
JustusOmbok <ombokjustus@gmail.com>
2023-08-29 10:52:43 -0700

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,10 +14,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 
 	left_height = binary_tree_height(tree->left);
-	right_hight = binary_tree_height(tree_right);
+	right_height = binary_tree_height(tree->right);
 
 	if (left_height > right_height)
-		return (left_height + 1);
+		return (left_height);
 	else
-		return (right_height + 1);
+		return (right_height);
 }
