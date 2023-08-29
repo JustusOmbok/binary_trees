86f4844be40dec07193a7e5f0f283df8001a6e44
JustusOmbok <ombokjustus@gmail.com>
2023-08-29 02:01:44 -0700

    betty corrections

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_node - A binary tree node is  created
-* @parent: Parent node pointer
-* @value: Value to be put in the new node
-
-* Return: New node pointer, or NULL when failure
-*/
+ * binary_tree_node - A binary tree node is  created
+ * @parent: Parent node pointer
+ * @value: Value to be put in the new node
+ *
+ * Return: New node pointer, or NULL when failure
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
