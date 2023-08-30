2875347aabac5b3c9dc4360ded4a70e4866c6204
JustusOmbok <ombokjustus@gmail.com>
2023-08-30 00:33:17 -0700

    node balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -33,13 +33,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 		return (0);
-
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	if (tree->left)
+		left_height = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		right_height = binary_tree_height(tree->right) + 1;
 
 	return (left_height - right_height);
 }
