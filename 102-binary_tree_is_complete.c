d5eb4863788bb86cac9ffa599dbb79ff5d30ae52
JustusOmbok <ombokjustus@gmail.com>
2023-08-31 02:42:24 -0700

    complete trees

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,39 +1,44 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_size - Measures size of binary tree
- * @tree: Pointer to root node
+ * nodes_count - Number of nodes are counted
+ * @tree: binary tree node
  *
- * Return: Size of tree or 0
+ * Return: nodes in the tree
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+int nodes_count(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	return (1 + binary_tree_size(tree->left) + binary_tree_size(tree->right));
+	return (1 + nodes_count(tree->left) + nodes_count(tree->right));
 }
 
 /**
- * binary_tree_is_complete - Checks if binary tree complete
- * @tree: Pointer to root node
+ * its_complete - Checks if tree is complete
+ * @tree: tree node
+ * @index: index of the tree
+ * @n: no of nodes
+ * Return: 1 on success, or 0
+ */
+int its_complete(const binary_tree_t *tree, int index, int n)
+{
+	if (!tree)
+		return (1);
+	if (index >= n)
+		return (0);
+	return (its_complete(tree->left, (2 * index) + 1, n) &&
+			its_complete(tree->right, (2 * index) + 2, n));
+}
+
+/**
+ * binary_tree_is_complete - Checks if tree is complete
+ * @tree: binary tree node
  *
- * Return: 1 if tree complete, or 0
+ * Return: 1 if complete, or 0
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-	size_t size, i;
+	int count_node = nodes_count(tree);
 
-	if (tree == NULL)
-		return (0);
-
-	size = binary_tree_size(tree);
-	
-	for (i = 0; i < size; i++)
-	{
-		if (!print_level(tree, NULL, i + 1))
-			return (0);
-	}
-
-	return (1);
+	return (its_complete(tree, 0, count_node));
 }
