618057e1e9dbb6a7476525d82fa6d9a7b6a30e9c
JustusOmbok <ombokjustus@gmail.com>
2023-08-30 23:33:22 -0700

    traversal

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -19,6 +19,7 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 	while (front < rear)
 	{
 		binary_tree_t *node = queue[front++];
+
 		func(node->n);
 
 		if (node->left)
